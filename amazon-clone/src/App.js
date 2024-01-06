a07c176ab8b1ca8df73b095910059fda9b411bea
chinmaypokhariyal <pokhariyal.chinmay@gmail.com>
2024-01-07 01:24:59 +0530

    header design and setup

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     <h1>Amazon Store </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firbase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDGLx08wT6gkB-L3DUn4oh3WboF1RJWVrA",
+    authDomain: "clone-dfd5e.firebaseapp.com",
+    projectId: "clone-dfd5e",
+    storageBucket: "clone-dfd5e.appspot.com",
+    messagingSenderId: "636047044924",
+    appId: "1:636047044924:web:2c0a9ca3564582067d2e39",
+    measurementId: "G-6DXSM5BM6D"
+  }; 
